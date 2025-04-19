f31dd4b357e8bdeb28c3f1124d9e5d81c2c6579b
Zitta <zmalekmakanph@gmail.com>
2025-04-19 15:11:25 +0330

    repeating player

--- src/components/MPlayer.tsx
+++ src/components/MPlayer.tsx
@@ -1,39 +1,42 @@
-"use client"
-import React, { useEffect, useRef } from "react";
+"use client";
+import { Repeat, Repeat1 } from "lucide-react";
+import React, { useEffect, useRef, useState } from "react";
 import H5AudioPlayer from "react-h5-audio-player";
 import "react-h5-audio-player/lib/styles.css";
 
 function MPlayer() {
-    const playerRef = useRef<H5AudioPlayer | null>(null);
-    useEffect(()=>{
-        const audio1 = playerRef.current?.audio.current;
-        if(audio1){
-            audio1.loop = true;
-            console.log("audio started");
+  const playerRef = useRef<any>(null);
 
+  const handleEnd = () => {
+    const audio = playerRef.current?.audio.current;
+    if (audio) {
+        try {
+          audio.currentTime = 0;
+          const playPromise = audio.play();
+          if (playPromise !== undefined) {
+            playPromise.catch((err: any) => {
+              console.warn("Auto-replay blocked", err);
+            });
+          }
+        } catch (err) {
+          console.log("error replaying", err);
         }
+    }
+  };
 
-    },[])
-    // const handleEnd = ()=>{
-    //     const audio = playerRef.current?.audio.current;
-    //     if(audio) {
-    //         audio.currentTime = 0;
-    //         audio.play();
-    //     }
-    // }
+
   return (
     <div className="w-full">
-      {/* <audio controls autoPlay className='w-full rounded-md'>
-            <source src='./musics/song.mp3' type='audio/mp3'/>
-        </audio> */}
+      <Repeat size={20} />
       <H5AudioPlayer
-        src="./musics/song.mp3"
+        ref={playerRef}
+        src="./musics/Idealism-Both-of-Us.mp3"
         autoPlay={true}
         showJumpControls={false}
         customAdditionalControls={[]}
         layout="horizontal"
         style={{ borderRadius: ".5rem", backgroundColor: "#1f2937" }}
-        // onEnded={handleEnd}
+        onEnded={handleEnd}
       />
     </div>
   );
